1bd48210d0d552dfd3cd1057b451cf509b37ce53
secoblgr <secoblgr52@gmail.com>
2023-03-26 18:13:51 +0300

    final push

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import getData from './App';          // index sayfamıza axios işlemini yaptığımız dosyamızı ekleiyoruz.
-getData(1)
+
+getData(1)           // 1  nolu id için  getData fonksiyonunu çagırıyoruz.
